de8730f6d6570a6de608571b34f9201f83a3a8c6
imamura-slab <atuuyarumami0704@yahoo.co.jp>
2021-08-25 17:07:02 +0900

    Update

--- src/visodo.cpp
+++ src/visodo.cpp
@@ -1,74 +1,10 @@
-/*
-
-  The MIT License
-
-  Copyright (c) 2015 Avi Singh
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-
-*/
-
 #include "vo_features.h"
 
 using namespace cv;
 using namespace std;
 
-//#define MAX_FRAME 1000
-//#define MIN_NUM_FEAT 2000
-
 #define MAX_FRAME 150
-#define MIN_NUM_FEAT 100
-
-
-
-// IMP: Change the file directories (4 places) according to where your dataset is saved before running!
-
-double getAbsoluteScale(int frame_id, int sequence_id, double z_cal)	{
-  
-  string line;
-  int i = 0;
-  ifstream myfile ("/home/users/imamura/WORK/Git/mono-vo/dataset/kitti/dataset/groundtruth/poses/00.txt");
-  double x=0, y=0, z=0;
-  double x_prev, y_prev, z_prev;
-  if (myfile.is_open()){
-      while (( getline (myfile,line) ) && (i<=frame_id))
-	{
-	  z_prev = z;
-	  x_prev = x;
-	  y_prev = y;
-	  std::istringstream in(line);
-	  //cout << line << '\n';
-	  for (int j=0; j<12; j++)  {
-	    in >> z ;
-	    if (j==7) y=z;
-	    if (j==3)  x=z;
-	  }
-	  i++;
-	}
-      myfile.close();
-  }else{
-    cout << "Unable to open file";
-    return 0;
-  }
-
-  return sqrt((x-x_prev)*(x-x_prev) + (y-y_prev)*(y-y_prev) + (z-z_prev)*(z-z_prev)) ;
-}
+#define MIN_NUM_FEAT 10
 
 
 int main( int argc, char** argv ){
@@ -76,19 +12,15 @@ int main( int argc, char** argv ){
   Mat R_f, t_f; //the final rotation and tranlation vectors containing the 
 
   ofstream myfile;
-  myfile.open ("results1_1.txt");
+  myfile.open("results1_1.txt");
 
   double scale = 1.00;
   char filename1[200];
   char filename2[200];
-  // sprintf(filename1, "/home/users/imamura/WORK/Git/mono-vo/dataset/kitti/dataset/sequences/00/image_0/%06d.png", 0); 
-  // sprintf(filename2, "/home/users/imamura/WORK/Git/mono-vo/dataset/kitti/dataset/sequences/00/image_0/%06d.png", 1);
 
   sprintf(filename1, "/home/users/imamura/WORK/Git/mono-vo/dataset/contest/run1/view_%08d.png", 0);
   sprintf(filename2, "/home/users/imamura/WORK/Git/mono-vo/dataset/contest/run1/view_%08d.png", 1);
 
-  
-  
   char text[100];
   int fontFace = FONT_HERSHEY_PLAIN;
   double fontScale = 1;
@@ -103,17 +35,28 @@ int main( int argc, char** argv ){
     std::cout<< " --(!) Error reading images " << std::endl; return -1;
   }
 
+  int miss_cnt=0;
+
+  
   // we work with grayscale images
   cvtColor(img_1_c, img_1, COLOR_BGR2GRAY);
   cvtColor(img_2_c, img_2, COLOR_BGR2GRAY);
 
   // feature detection, tracking
-  vector<Point2f> points1, points2;        //vectors to store the coordinates of the feature points
-  featureDetection(img_1, points1);        //detect features in img_1
-  vector<uchar> status;
-  featureTracking(img_1,img_2,points1,points2, status); //track those features to img_2
+  vector<Point2f> points1, points2;  //vectors to store the coordinates of the feature points
+  
+  // vector<KeyPoint> の参照渡しはうまくいかない??????????
+  vector<KeyPoint> key1, key2;
+  vector<DMatch> match;
+  tie(key1, key2, match) = feature_matching(img_1, img_2);
 
+  tie(points1, points2) = sort_feature(key1, key2, match);
+  // KeyPoint::convert(key1, points1, vector<int>());
+  // KeyPoint::convert(key2, points2, vector<int>());
 
+  cout << "-------------------------------\n";
+  cout << points1.size() << ", " << points2.size() << endl;
+  
   // KITTI
   // double focal = 718.8560;
   // cv::Point2d pp(607.1928, 185.2157);
@@ -122,11 +65,10 @@ int main( int argc, char** argv ){
   double focal = 1020.05;
   cv::Point2d pp(721.803, 512.326);
 
-  // double focal = 2.92755;
-  // cv::Point2d pp(2.07157, 1.9627);
   
-  
-  //recovering the pose and the essential matrix
+  ////recovering the pose and the essential matrix
+  // R : 回転行列 3x3
+  // t : 並進行列 1x3
   Mat E, R, t, mask;
   E = findEssentialMat(points2, points1, focal, pp, RANSAC, 0.999, 1.0, mask);
   recoverPose(E, points2, points1, R, t, focal, pp, mask);
@@ -146,21 +88,44 @@ int main( int argc, char** argv ){
   namedWindow( "Road facing camera", WINDOW_AUTOSIZE );// Create a window for display.
   namedWindow( "Trajectory", WINDOW_AUTOSIZE );// Create a window for display.
 
-  Mat traj = Mat::zeros(600, 600, CV_8UC3);
-
+  //Mat traj = Mat::zeros(600, 600, CV_8UC3);
+  Mat traj = imread("../map.png"); // 350x490
+  
   for(int numFrame=2; numFrame < MAX_FRAME; numFrame++)	{
     sprintf(filename, "/home/users/imamura/WORK/Git/mono-vo/dataset/contest/run1/view_%08d.png", numFrame);
-    //cout << numFrame << endl;
+    cout << "\nframe: " << numFrame << endl;
     Mat currImage_c = imread(filename);
     cvtColor(currImage_c, currImage, COLOR_BGR2GRAY);
-    vector<uchar> status;
-    featureTracking(prevImage, currImage, prevFeatures, currFeatures, status);
+    tie(key1, key2, match) = feature_matching(prevImage, currImage);
+    tie(points1, points2) = sort_feature(key1, key2, match);
+    // KeyPoint::convert(key1, prevFeatures, vector<int>());
+    // KeyPoint::convert(key2, currFeatures, vector<int>());
+
     
+    cout << "points1 size: " << points1.size() << ", points2 size: " << points2.size() << ", match size: "<< match.size() << endl;
+    for(int i=0; i<points1.size(); i++){
+      cout << points1.at(i).x << ", " << points1.at(i).y << endl;
+    }
+    cout << "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n";
+    
+    Mat dst_img;
+    drawMatches(prevImage, key1, currImage, key2, match, dst_img);
+    imshow("match", dst_img);
+    
+    if(match.size() <= 5){
+      cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n";
+      cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n";
+      cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n";
+      cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n";
+      cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n";
+    }
     E = findEssentialMat(currFeatures, prevFeatures, focal, pp, RANSAC, 0.999, 1.0, mask);
     recoverPose(E, currFeatures, prevFeatures, R, t, focal, pp, mask);
     
     Mat prevPts(2,prevFeatures.size(), CV_64F), currPts(2,currFeatures.size(), CV_64F);
 
+    Mat currImage_draw = currImage.clone();
+    Mat prevImage_draw = prevImage.clone();
     
     for(int i=0;i<prevFeatures.size();i++){  //this (x,y) combination makes sense as observed from the source code of triangulatePoints on GitHub
       prevPts.at<double>(0,i) = prevFeatures.at(i).x;
@@ -170,50 +135,48 @@ int main( int argc, char** argv ){
       currPts.at<double>(1,i) = currFeatures.at(i).y;
     }
     
-    //scale = getAbsoluteScale(numFrame, 0, t.at<double>(2));
-
-    //cout << "Scale is " << scale << endl;
-
+    scale = 10.0;
     if((scale>0.1)&&(t.at<double>(2) > t.at<double>(0)) && (t.at<double>(2) > t.at<double>(1))){
       t_f = t_f + scale*(R_f*t);
       R_f = R*R_f;
     }else{
-      //cout << "scale below 0.1, or incorrect translation" << endl;
+      miss_cnt++;
+      cout << "scale below 0.1, or incorrect translation. numFrame: " << numFrame << endl;
+      // cout << t << endl;
+      // cout << R << endl;
     }
     
     // lines for printing results
-    // myfile << t_f.at<double>(0) << " " << t_f.at<double>(1) << " " << t_f.at<double>(2) << endl;
+    myfile << t_f.at<double>(0) << " " << t_f.at<double>(1) << " " << t_f.at<double>(2) << endl;
 
     // a redetection is triggered in case the number of feautres being trakced go below a particular threshold
     if (prevFeatures.size() < MIN_NUM_FEAT){
-      //cout << "Number of tracked features reduced to " << prevFeatures.size() << endl;
-      //cout << "trigerring redection" << endl;
-      featureDetection(prevImage, prevFeatures);
-      featureTracking(prevImage,currImage,prevFeatures,currFeatures, status);
+      tie(key1, key2, match) = feature_matching(prevImage, currImage);
+      tie(points1, points2) = sort_feature(key1, key2, match);
+      // KeyPoint::convert(key1, prevFeatures, vector<int>());
+      // KeyPoint::convert(key2, currFeatures, vector<int>());
     }
 
     prevImage = currImage.clone();
     prevFeatures = currFeatures;
 
-    int x = int(t_f.at<double>(0)) + 300;
-    int y = int(t_f.at<double>(2)) + 100;
+    int x = int(t_f.at<double>(0)) + 18;
+    int y = int(t_f.at<double>(2)) + 462;
     circle(traj, Point(x, y) ,1, CV_RGB(255,0,0), 2);
 
-    rectangle( traj, Point(10, 30), Point(550, 50), CV_RGB(0,0,0), CV_FILLED);
-    sprintf(text, "Coordinates: x = %02fm y = %02fm z = %02fm", t_f.at<double>(0), t_f.at<double>(1), t_f.at<double>(2));
-    putText(traj, text, textOrg, fontFace, fontScale, Scalar::all(255), thickness, 8);
 
-    imshow( "Road facing camera", currImage_c );
+    //imshow( "Road facing camera", currImage_c );
     imshow( "Trajectory", traj );
 
-    waitKey(200);
+    waitKey(20);
 
   }
 
   clock_t end = clock();
   double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
   cout << "Total time taken: " << elapsed_secs << "s" << endl;
-
+  cout << "miss count: " << miss_cnt << endl;
+  
   //cout << R_f << endl;
   //cout << t_f << endl;
 
--- src/vo_features.h
+++ src/vo_features.h
@@ -56,24 +56,163 @@ void featureTracking(Mat img_1, Mat img_2, vector<Point2f>& points1, vector<Poin
 
   //getting rid of points for which the KLT tracking failed or those who have gone outside the frame
   int indexCorrection = 0;
-  for( int i=0; i<status.size(); i++)
-    {  Point2f pt = points2.at(i- indexCorrection);
-      if ((status.at(i) == 0)||(pt.x<0)||(pt.y<0)){
-	if((pt.x<0)||(pt.y<0)){
-	  status.at(i) = 0;
-	}
-	points1.erase (points1.begin() + (i - indexCorrection));
-	points2.erase (points2.begin() + (i - indexCorrection));
-	indexCorrection++;
+  for( int i=0; i<status.size(); i++){
+    Point2f pt = points2.at(i- indexCorrection);
+    if ((status.at(i) == 0)||(pt.x<0)||(pt.y<0)){
+      if((pt.x<0)||(pt.y<0)){
+  	status.at(i) = 0;
       }
+      points1.erase (points1.begin() + (i - indexCorrection));
+      points2.erase (points2.begin() + (i - indexCorrection));
+      indexCorrection++;
     }
+  }
+  
 }
 
 
 void featureDetection(Mat img_1, vector<Point2f>& points1){   //uses FAST as of now, modify parameters as necessary
   vector<KeyPoint> keypoints_1;
+
+  // FAST
   int fast_threshold = 20;
   bool nonmaxSuppression = true;
   FAST(img_1, keypoints_1, fast_threshold, nonmaxSuppression);
   KeyPoint::convert(keypoints_1, points1, vector<int>());
+
+
+  // draw
+  /* Mat dst = Mat::zeros(img_1.rows, img_1.cols, CV_8UC3); */
+  /* drawKeypoints(img_1, keypoints_1, dst); */
+  /* imshow("FAST", dst); */
+
+  // 
 }
+
+
+
+vector<KeyPoint> feature_detection(Mat img){
+  Mat dst_img;
+  vector<KeyPoint> keypoint;
+  
+  // A-KAZE
+  Ptr<cv::AKAZE> akaze = AKAZE::create(AKAZE::DESCRIPTOR_MLDB, 0, 3, 0.001f);
+  akaze->detect(img, keypoint);
+
+  // ORB
+  /* Ptr<cv::ORB> orb = ORB::create(500, 1.2f, 2); */
+  /* orb->detect(img, keypoint); */
+  
+  
+  drawKeypoints(img, keypoint, dst_img, Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+  /* imshow("feature_img", dst_img); */
+  /* waitKey(); */
+
+  return keypoint;
+}
+
+
+  
+tuple<vector<KeyPoint>, vector<KeyPoint>, vector<DMatch>> feature_matching(Mat img_1, Mat img_2){
+  Mat dst_img;
+  vector<KeyPoint> key1, key2;
+  Mat des1, des2;
+  
+  //// キーポイントの検出 & 特徴量記述の計算
+  // A-KAZE
+  /* Ptr<AKAZE> akaze = AKAZE::create(AKAZE::DESCRIPTOR_MLDB, 0, 3, 0.001f); */
+  /* akaze->detect(img_1, key1); */
+  /* akaze->detect(img_2, key2); */
+  /* akaze->compute(img_1, key1, des1); */
+  /* akaze->compute(img_2, key2, des2); */
+  
+
+  // ORB
+  Ptr<ORB> orb = ORB::create(30);
+  orb->detect(img_1, key1);
+  orb->detect(img_2, key2);
+  orb->compute(img_1, key1, des1);
+  orb->compute(img_2, key2, des2);
+
+
+  // version問題?
+  /* Ptr<FeatureDetector> detector = FeatureDetector::create("ORB"); */
+  /* Ptr<DescriptorExtractor> descriptorExtractor = DescriptorExtractor::create("ORB"); */
+  /* Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce"); */
+  /* detector->detect(img_1, key1); */
+  /* detector->detect(img_2, key2); */
+  /* descriptorExtractor->compute(img_1, key1, des1); */
+  /* descriptorExtractor->compute(img_2, key2, des2); */
+
+  
+  // 特徴量マッチングアルゴリズムの選択  
+  Ptr<DescriptorMatcher> hamming = DescriptorMatcher::create("BruteForce-Hamming");
+
+  // 特徴量マッチング
+  vector<DMatch> match;
+  vector<DMatch> match12, match21;
+  hamming->match(des1, des2, match12);
+  hamming->match(des2, des1, match21);
+
+  for(size_t i=0; i<match12.size(); i++){
+    DMatch m12 = match12[i];
+    DMatch m21 = match21[m12.trainIdx];
+
+    if(m21.trainIdx == m12.queryIdx)
+      match.push_back(m12);
+  }
+  cout << "++++++++++++++++++++++++++++++++++\n";
+  cout << "m12 <--> m21 match: " << match.size() << endl;
+
+
+  // 特徴量距離が小さい順にソート
+  for(int i=0; i<match.size()-1; i++){
+    double min = match[i].distance;
+    int n = i;
+    for(int j=i+1; j<match.size(); j++){
+      if(match[j].distance < min){
+	n = j;
+	min = match[j].distance;
+      }
+    }
+    swap(match[i], match[n]);
+  }
+  // 上位n点を残して, 残りを削除
+  int n = 20;
+  if(match.size() > n)
+    match.erase(match.begin()+n, match.end());
+
+
+  
+  // draw
+  // drawMatches(img_1, key1, img_2, key2, match, dst_img);
+
+  /* imshow("match feature", dst_img); */
+  /* waitKey(); */
+
+
+  return forward_as_tuple(key1, key2, match);
+}
+
+
+
+tuple<vector<Point2f>, vector<Point2f>> sort_feature(vector<KeyPoint> key1, vector<KeyPoint> key2, vector<DMatch> match){
+  vector<Point2f> points1, points2;
+
+  for(size_t i=0; i<match.size(); i++){
+    Point2f p;
+
+    p.x = key1[match[i].queryIdx].pt.x;
+    p.y = key1[match[i].queryIdx].pt.y;
+    points1.push_back(p);
+
+    p.x = key2[match[i].trainIdx].pt.x;
+    p.y = key2[match[i].trainIdx].pt.y;
+    points2.push_back(p);
+  }
+  
+  return forward_as_tuple(points1, points2);
+}
+
+
